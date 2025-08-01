aa463c3a58199719bb46a864b47275f2a2b75aee
JaqueBRSilva <j.silvabroccolo@gmail.com>
2025-07-31 23:22:57 -0300

    feat: criado página `Error404`

---
+++ src/pages/404/Error404.jsx
@@ -0,0 +1,12 @@
+export default function Error404() {
+    return (
+        <>
+            <h1>
+                404
+            </h1>
+            <h2>
+                Pagina Não Encontrada
+            </h2>
+        </>
+    )
+}
