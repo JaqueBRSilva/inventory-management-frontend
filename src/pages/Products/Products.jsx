7044b89d41dbef13b1492bc983fe9abd5783c65d
JaqueBRSilva <j.silvabroccolo@gmail.com>
2025-08-01 23:07:25 -0300

    feat: add campos e validação p/ produtos

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,42 +1,117 @@
-import { useState } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import { useForm } from 'react-hook-form';
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
 import TableProd from '../../components/TableProd/TableProd';
 
-const list = [
-    { id: 1, nome: "Notebook Dell", quantidade: 5, preco: 4500.00 },
-    { id: 2, nome: "Mouse Logitech", quantidade: 20, preco: 150.00 },
-    { id: 3, nome: "Teclado Mecânico", quantidade: 10, preco: 350.00 },
-    { id: 4, nome: "Monitor Samsung", quantidade: 7, preco: 1200.00 },
-    { id: 5, nome: "Impressora HP", quantidade: 4, preco: 980.00 },
-    { id: 6, nome: "HD Externo 1TB", quantidade: 15, preco: 390.00 },
-    { id: 7, nome: "Webcam Full HD", quantidade: 8, preco: 250.00 },
-    { id: 8, nome: "Cadeira Gamer", quantidade: 3, preco: 1300.00 },
-    { id: 9, nome: "Smartphone Xiaomi", quantidade: 12, preco: 1800.00 },
-    { id: 10, nome: "Caixa de Som Bluetooth", quantidade: 9, preco: 220.00 }
-]
+
+const nameValid = {
+    required: {
+        value: true,
+        message: "Preencha o nome do produto"
+    },
+    maxLength: {
+        value: 25,
+        message: "O nome do produto deve ter no máximo 25 caracteres"
+    }
+}
+
+const quantityValid = {
+    required: {
+        value: true,
+        message: "Insira uma quantidade"
+    },
+    min: {
+        value: 1,
+        message: "A quantidade tem que ser pelo menos 1"
+    }
+}
+
+const priceValid = {
+    required: {
+        value: true,
+        message: "Valor precisa ser inserido"
+    },
+    min: {
+        value: 0.0001,
+        message: "O preço não pode ser menor ou igual a 0"
+    }
+}
 
 export default function Products() {
-    const [products, setProducts] = useState(list)
+    const [products, setProducts] = useState([])
+    const { register, handleSubmit, formState: { errors } } = useForm()
+
+    function onSubmit(formData) {
+        formData.id = products.length
+        setProducts([...products, formData])
+    }
+
+    async function getProductsList() {
+        const url = "https://inventory-management-api-o8x3.onrender.com/produtos"
+        const response = await axios.get(url)
+        setProducts(response.data)
+    }
+
+    useEffect(() => {
+        getProductsList()
+    }, [])
 
     return (
         <>
             <Header />
+            <Container className="my-5">
+                <h1>Produtos</h1>
+
+                <Form noValidate onSubmit={handleSubmit(onSubmit)}>
+                    <Row className="mb-3">
+                        <Form.Group as={Col} md="12" controlId="name">
+                            <Form.Label>Nome do Produto</Form.Label>
+                            <Form.Control type="text" isInvalid={!!errors.name} {...register("name", nameValid)} />
+                            <Form.Control.Feedback type="invalid">
+                                {errors.name?.message}
+                            </Form.Control.Feedback>
+                        </Form.Group>
+
+                        <Form.Group as={Col} md="6" controlId="quantity">
+                            <Form.Label>Quantidade</Form.Label>
+                            <Form.Control type="number" isInvalid={!!errors.quantity} {...register("quantity", quantityValid)} />
+                            <Form.Control.Feedback type="invalid">
+                                {errors.quantity?.message}
+                            </Form.Control.Feedback>
+                        </Form.Group>
+
+                        <Form.Group as={Col} md="6" controlId="price">
+                            <Form.Label>Preço</Form.Label>
+                            <Form.Control type="number" isInvalid={!!errors.price} {...register("price", priceValid)} />
+                            <Form.Control.Feedback type="invalid" >
+                                {errors.price?.message}
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                    </Row>
+                    <Button type="submit" >Cadastrar</Button>
+                </Form>
+            </Container>
+
             <Container className='py-5'>
-                <h1>Lista de Produtos</h1>
+                <h2>Lista de Produtos</h2>
 
                 {(products.length > 0) ? (
 
-                    <TableProd
-                        products={products}
-                    />
+                    <TableProd products={products} />
 
                 ) : (
 
                     <>
                         <p>Nenhum produto disponível no momento</p>
                     </>
+
                 )}
             </Container>
             <Footer />
