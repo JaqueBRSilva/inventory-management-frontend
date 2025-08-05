765c03cabe8cb0eef4313deee5d9fb87b75f88d8
JaqueBRSilva <j.silvabroccolo@gmail.com>
2025-08-05 11:56:12 -0300

    feat: incluído 'AuthContext' em App.jsx,

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import { AuthProvider } from './context/AuthContext'
 import Error404 from './pages/404/Error404'
 import HomePage from './pages/Home/Home'
+import Login from './pages/Login/Login'
 import Products from './pages/Products/Products'
 import Users from './pages/Users/Users'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/users" element={<Users />} />
-        <Route path="*" element={<Error404 />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+      </BrowserRouter>
+
+    </AuthProvider>
   )
 }
 
